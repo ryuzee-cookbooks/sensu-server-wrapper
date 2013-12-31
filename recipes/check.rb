20c01eb996e5baa380ebbe124f9b3619fcc6bf30
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-12-31 13:37:32 +0900

    add check

--- recipes/check.rb
+++ recipes/check.rb
@@ -21,6 +21,13 @@ template "/etc/sensu/conf.d/check_cron.json" do
   source "check_cron.json.erb"
 end
 
+template "/etc/sensu/conf.d/check-disk.json" do
+  owner "sensu"
+  group "sensu"
+  mode  0755
+  source "check-disk.json.erb"
+end
+
 template "/etc/sensu/conf.d/load-metrics.json" do
   owner "sensu"
   group "sensu"
