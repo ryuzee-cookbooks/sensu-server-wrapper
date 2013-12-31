5e4b7a321c23e82d986ab36fd8d5c03419464c26
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-12-31 10:36:14 +0900

    fix metadata issue

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 depends          'iptables'
 depends          'yum', "~> 3.0.4"
+depends          'yum-epel'
 depends          'sensu'
