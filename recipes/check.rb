3b54764672f5c113e5af9043627274ad4ab50fcd
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-01-02 13:19:51 +0900

    graphite support

--- recipes/check.rb
+++ recipes/check.rb
@@ -49,6 +49,24 @@ template "/etc/sensu/conf.d/load-metrics.json" do
   source "load-metrics.json.erb"
 end
 
+remote_file "/etc/sensu/plugins/graphite.rb" do
+  source "https://github.com/sensu/sensu-community-plugins/raw/master/mutators/graphite.rb"
+end
+
+template "/etc/sensu/conf.d/handler-graphite.json" do
+  owner "sensu"
+  group "sensu"
+  mode  0755
+  source "handler-graphite.json.erb"
+end
+
+template "/etc/sensu/conf.d/mutator-graphite.json" do
+  owner "sensu"
+  group "sensu"
+  mode  0755
+  source "mutator-graphite.json.erb"
+end
+
 service "sensu-server" do
   action :restart
 end
