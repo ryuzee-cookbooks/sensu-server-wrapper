b013a79f9857b34b150733ecbca3611c86ff1517
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-02-20 21:03:05 +0900

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'ryuzee@gmail.com'
 license          'MIT'
 description      'Installs/Configures sensu-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 depends          'iptables'
 depends          'yum', "~> 3.0.4"
 depends          'yum-epel'
-depends          'sensu'
+depends          'sensu', '~> 0.8.0'
