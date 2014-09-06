a54f7424250434ac32f6b7d206d50d0abff8770c
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-09-06 12:16:10 +0900

    fix yum version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures sensu-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.0'
 depends          'iptables'
-depends          'yum', "~> 3.0.4"
+depends          'yum'
 depends          'yum-epel'
 depends          'sensu', "~> 1.0.0"
 depends          'apache2-simple'
