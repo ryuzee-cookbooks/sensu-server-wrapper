d5e71425abaefe12e679feba31c1a349cd699734
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-04-16 06:33:32 +0900

    Bumped

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ryuzee@gmail.com'
 license          'MIT'
 description      'Installs/Configures sensu-server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.0'
+version          '0.5.1'
 depends          'ca-certificates'
 depends          'iptables'
 depends          'yum'
