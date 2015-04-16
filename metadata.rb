daf9b936c92030a2b5f60a5ac5ab49141b0d58d2
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-04-16 21:55:41 +0900

    add dependency

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.5.2'
 depends          'ca-certificates'
 depends          'iptables'
-depends          'yum'
+depends          'yum', "~> 3.5"
 depends          'yum-epel'
 depends          'sensu', "~> 2.2"
 depends          "redisio", "~> 1.7"
